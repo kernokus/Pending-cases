a380231121e2a1c91cafdee93cdb59ea969f8c0f
Anatoliy <tol1017@ya.ru>
2020-06-26 19:35:19 +0500

    эксперимент с ML(не Firebase), доработка дизайна настроек

--- app/src/main/java/com/example/testapplication/MLActivity.kt
+++ app/src/main/java/com/example/testapplication/MLActivity.kt
@@ -1,37 +1,29 @@
 package com.example.testapplication
-import android.annotation.SuppressLint
+
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.util.SparseIntArray
 import android.view.Surface
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
-
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.objects.ObjectDetection
-import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
-import com.google.mlkit.vision.objects.defaults.PredefinedCategory
 import kotlinx.android.synthetic.main.activity_ml.*
 import timber.log.Timber
 import java.io.File
-import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-typealias LumaListener = (luma: Double) -> Unit
+
 class MLActivity: AppCompatActivity() {
     companion object {
-        private const val TAG = "CameraXBasic"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
@@ -40,7 +32,6 @@ class MLActivity: AppCompatActivity() {
     private val ORIENTATIONS = SparseIntArray()
     private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
-    var imageAnalyzer: ImageAnalysis? = null
     private var camera: Camera? = null
 
     private lateinit var outputDirectory: File
@@ -53,18 +44,6 @@ class MLActivity: AppCompatActivity() {
         ORIENTATIONS.append(Surface.ROTATION_270, 180)
     }
 
-
-
-    // Multiple object detection in static images
-    val optionsStatic = ObjectDetectorOptions.Builder()
-        .setDetectorMode(ObjectDetectorOptions.SINGLE_IMAGE_MODE)
-        .enableMultipleObjects()
-        .enableClassification()  // Optional
-        .build()
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ml)
@@ -76,17 +55,11 @@ class MLActivity: AppCompatActivity() {
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
             )
         }
-
-        // Setup the listener for take photo button
         camera_capture_button.setOnClickListener {
             takePhoto()
         }
-
         outputDirectory = getOutputDirectory()
-
         cameraExecutor = Executors.newSingleThreadExecutor()
-
-
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -97,50 +70,21 @@ class MLActivity: AppCompatActivity() {
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
         cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
             preview = Preview.Builder()
                 .build()
 
             imageCapture = ImageCapture.Builder()
                 .build()
 
-
-
-            imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, YourImageAnalyzer {
-                    })
-                }
-
-            imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-
-                        Log.d(TAG, "Average luminosity: $luma")
-                        runOnUiThread {
-                            Toast.makeText(baseContext, luma.toString(), Toast.LENGTH_SHORT).show() // работает с большой задержкой,что логично
-                        }
-                    })
-                }
-
-            // Select back camera
             val cameraSelector =
                 CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
 
             try {
-                // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
-
-                // Bind use cases to camera
                 camera = cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, imageAnalyzer
+                    this, cameraSelector, preview, imageCapture
                 )
                 preview?.setSurfaceProvider(viewFinder.createSurfaceProvider(camera?.cameraInfo))
             } catch (exc: Exception) {
@@ -151,22 +95,14 @@ class MLActivity: AppCompatActivity() {
     }
 
     private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
         val imageCapture = imageCapture ?: return
-
-        // Create timestamped output file to hold the image
         val photoFile = File(
             outputDirectory,
             SimpleDateFormat(
                 FILENAME_FORMAT, Locale.US
             ).format(System.currentTimeMillis()) + ".jpg"
         )
-
-        // Create output options object which contains file + metadata
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        // Setup image capture listener which is triggered after photo has
-        // been taken
         imageCapture.takePicture(
             outputOptions,
             ContextCompat.getMainExecutor(this),
@@ -174,7 +110,6 @@ class MLActivity: AppCompatActivity() {
                 override fun onError(exc: ImageCaptureException) {
 
                 }
-
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     val savedUri = Uri.fromFile(photoFile)
                     val msg = "Photo capture succeeded: $savedUri"
@@ -194,7 +129,6 @@ class MLActivity: AppCompatActivity() {
             mediaDir else filesDir
     }
 
-
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults:
         IntArray
@@ -212,74 +146,4 @@ class MLActivity: AppCompatActivity() {
             }
         }
     }
-
-    private class YourImageAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
-
-        @SuppressLint("UnsafeExperimentalUsageError")
-        override fun analyze(imageProxy: ImageProxy) {
-            val mediaImage = imageProxy.image
-            if (mediaImage != null) {
-                val image =
-                    InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-                // Pass image to an ML Kit Vision API
-                // Live detection and tracking
-                val optionsLive = ObjectDetectorOptions.Builder()
-                    .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
-                    .enableClassification()  // Optional
-                    .build()
-                val objectDetector = ObjectDetection.getClient(optionsLive)
-                objectDetector.process(image)
-                    .addOnSuccessListener {
-                        for (detectedObject in it) {
-
-                            val boundingBox = detectedObject.boundingBox
-                            val trackingId = detectedObject.trackingId
-                            for (label in detectedObject.labels) {
-                                val text = label.text
-                                if (PredefinedCategory.FOOD == text) {
-                                   Timber.d(text)
-                                }
-                                val index = label.index
-                                if (PredefinedCategory.FOOD_INDEX == index) {
-                                    Timber.d(index.toString())
-                                }
-                                val confidence = label.confidence
-                            }
-                        }
-                        // Task completed successfully
-                        Timber.d("SUCCES")
-                    }
-                    .addOnFailureListener { e ->
-                        Timber.d("NOTSUCCES")
-                    }
-
-            }
-        }
-    }
-
-
-
-
-    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
-
-        private fun ByteBuffer.toByteArray(): ByteArray {
-            rewind()    // Rewind the buffer to zero
-            val data = ByteArray(remaining())
-            get(data)   // Copy the buffer into a byte array
-            return data // Return the byte array
-        }
-
-        override fun analyze(image: ImageProxy) {
-
-            val buffer = image.planes[0].buffer
-            val data = buffer.toByteArray()
-            val pixels = data.map { it.toInt() and 0xFF }
-            val luma = pixels.average()
-            listener(luma)
-            image.close()
-        }
-
-
-    }
-
 }
--- app/src/main/java/com/example/testapplication/SettingsActivity.kt
+++ app/src/main/java/com/example/testapplication/SettingsActivity.kt
@@ -1,12 +1,26 @@
 package com.example.testapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import butterknife.ButterKnife
+import butterknife.OnClick
 
 
 class SettingsActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ButterKnife.bind(this)
         setContentView(R.layout.settings)
+    }
+
+
+    @OnClick(R.id.LanguageConstraint,R.id.ColorConstraint)
+    fun click(view : View?){
+        when(view?.id){
+            R.id.LanguageConstraint -> {}
+            R.id.ColorConstraint-> {}
+
+        }
     }
 }
